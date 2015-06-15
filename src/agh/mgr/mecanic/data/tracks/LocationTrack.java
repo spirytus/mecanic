c07bfd903f34323bf61950aa83d90c52e3cb5312
Maciej Marczynski <maciej.marczynski@ubnt.com>
2015-06-15 23:21:41 +0200

    moar tracks

--- src/agh/mgr/mecanic/data/tracks/LocationTrack.java
+++ src/agh/mgr/mecanic/data/tracks/LocationTrack.java
@@ -1,4 +1,20 @@
 package agh.mgr.mecanic.data.tracks;
 
-public class LocationTrack {
+import agh.mgr.mecanic.data.Properties;
+import agh.mgr.mecanic.data.simple.IVector;
+import agh.mgr.mecanic.data.simple.VelocityVector;
+import pl.edu.agh.amber.location.LocationCurrent;
+
+public class LocationTrack extends BaseTrack {
+
+    public LocationTrack() {
+        IVector[] a = new VelocityVector[]{
+                new VelocityVector(Properties.BASE_VX, Properties.BASE_VY, Properties.BASE_WZ),
+                new VelocityVector(Properties.BASE_VX, -Properties.BASE_VY, -Properties.BASE_WZ),
+                new VelocityVector(Properties.BASE_VX, Properties.BASE_VY, Properties.BASE_WZ),
+                new VelocityVector(Properties.BASE_VX, -Properties.BASE_VY, -Properties.BASE_WZ),
+                new VelocityVector(Properties.BASE_VX, Properties.BASE_VY, Properties.BASE_WZ)
+        };
+        this.track = a;
+    };
 }
--- src/agh/mgr/mecanic/data/tracks/PaperTrack.java
+++ src/agh/mgr/mecanic/data/tracks/PaperTrack.java
@@ -7,13 +7,15 @@ public class PaperTrack extends BaseTrack {
     public PaperTrack(){
         this.interval = 4000;
 
-        double BASE_V=200.0;
+        double BASE_V=300.0;
         // public WheelsVelocity(double leftFront, double rightFront, double leftBack, double rightBack){
         IVector[] a = new WheelsVelocity[]{
-                new WheelsVelocity(BASE_V, BASE_V, BASE_V, BASE_V),
-                new WheelsVelocity(BASE_V, BASE_V, BASE_V, BASE_V),
-                new WheelsVelocity(BASE_V, BASE_V, BASE_V, BASE_V),
-                new WheelsVelocity(BASE_V, BASE_V, BASE_V, BASE_V),
+                //new WheelsVelocity(BASE_V, -BASE_V, BASE_V, -BASE_V),
+                //new WheelsVelocity(-BASE_V, BASE_V, -BASE_V, BASE_V),
+                //new WheelsVelocity(2*BASE_V, 2*BASE_V, BASE_V, BASE_V),
+                //new WheelsVelocity(BASE_V, BASE_V, 2*BASE_V, 2*BASE_V),
+                new WheelsVelocity(2*BASE_V, -0.5*BASE_V, 0, 0),
+                new WheelsVelocity(-2*BASE_V, 0.5*BASE_V, 0, 0)
         };
         this.track = a;
     }
