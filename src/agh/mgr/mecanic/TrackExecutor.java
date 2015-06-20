3771f8b1d5e0f28d5acff7a526418a847df81d80
Maciej Marczynski <maciej.marczynski@ubnt.com>
2015-06-20 15:07:43 +0200

    Drive robot and calculate position simutaneously

--- src/agh/mgr/mecanic/TrackExecutor.java
+++ src/agh/mgr/mecanic/TrackExecutor.java
@@ -4,21 +4,52 @@ import agh.mgr.mecanic.data.simple.IVector;
 import agh.mgr.mecanic.data.tracks.BaseTrack;
 import agh.mgr.mecanic.data.tracks.SimpleTrack;
 import pl.edu.agh.amber.common.AmberClient;
+import pl.edu.agh.amber.common.CyclicDataListener;
+import pl.edu.agh.amber.hokuyo.HokuyoProxy;
+import pl.edu.agh.amber.hokuyo.MapPoint;
+import pl.edu.agh.amber.hokuyo.Scan;
 import pl.edu.agh.amber.roboclaw.RoboclawProxy;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.List;
 
 public class TrackExecutor {
-    public static void execute(BaseTrack track, int commandResolution) {
+    public static void execute(BaseTrack track, int commandResolution){
 
-        final AmberClient client;
+        AmberClient client;
         try {
             client = new AmberClient("192.168.2.209", 26233);
         } catch (IOException e) {
             System.out.println("Unable to connect to robot: " + e);
             return;
         }
+
         final RoboclawProxy roboclawProxy = new RoboclawProxy(client, 0);
+        final HokuyoProxy hokuyoProxy = new HokuyoProxy(client, 0);
+
+        Thread hokuyoThread = null;
+            hokuyoThread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        while(true){
+                            Scan singleScan = hokuyoProxy.getSingleScan();
+
+                            List<MapPoint> mapPoints = singleScan.getPoints();
+                            List<List<Integer>> edges = Positioner.findEdges(mapPoints);
+                            Positioner.printDistancesToWall(mapPoints, edges);
+
+                        }
+                        } catch (IOException e) {
+                        e.printStackTrace();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+
+        hokuyoThread.start();
 
         IVector[] velocityVectors = track.getTrack();
         int interval = track.getInterval();
@@ -40,5 +71,6 @@ public class TrackExecutor {
         } finally {
             client.terminate();
         }
+        
     }
 }
